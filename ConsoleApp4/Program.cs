a7ef3be39af79a48249a2264a98fe22520aa0c98
Edilson Alves Jr <Edilson.alves@checkexpress.local>
2018-12-03 11:38:47 -0200

    TestGitProject

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+public class Sample
+{
+    public static void Main()
+    {
+        //Create the XmlDocument.
+        XmlDocument doc = new XmlDocument();
+        doc.Load("XMLFile1.xml");
+
+        //Display all the book titles.
+        XmlNodeList elemList = doc.GetElementsByTagName("CNPJ");
+        for (int i = 0; i < elemList.Count; i++)
+        {
+            Console.WriteLine(elemList[i].InnerXml);
+        }
+        Console.ReadKey();
+
+    }
+}
